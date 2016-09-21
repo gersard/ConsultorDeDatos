e6b1c157e8a05206ac96c734a4041233f173c303
Gerardo <gerardo.mascayano@gmail.com>
2016-09-20 23:50:11 -0300

    Configuracion del campo EditText para ingreso del valor y su cambio de Hint segun el fragment en el cual se esta posicionado

--- app/src/main/java/com/example/gerardo/apidatos/ui/MainActivity.java
+++ app/src/main/java/com/example/gerardo/apidatos/ui/MainActivity.java
@@ -6,8 +6,11 @@ import android.support.v4.app.Fragment;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.EditText;
+import android.widget.ImageButton;
 
 import com.example.gerardo.apidatos.PagerAdapter;
 import com.example.gerardo.apidatos.R;
@@ -16,14 +19,19 @@ import java.util.ArrayList;
 
 import butterknife.Bind;
 import butterknife.ButterKnife;
+import butterknife.OnClick;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements ViewPager.OnPageChangeListener {
 
 
     @Bind(R.id.viewPager)
     ViewPager viewPager;
     @Bind(R.id.tabLayout)
     TabLayout tabLayout;
+    @Bind(R.id.edit_value)
+    EditText editValue;
+    @Bind(R.id.btn_buscar)
+    ImageButton btnBuscar;
 
 
     @Override
@@ -33,21 +41,25 @@ public class MainActivity extends AppCompatActivity {
         ButterKnife.bind(this);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayShowTitleEnabled(false);
 
         setupViewPager(true);
+        viewPager.addOnPageChangeListener(this);
+        //Setea a que fragment queremos dejar que aparesca al inicio
+//        viewPager.setCurrentItem(1,true);
 
     }
 
-    private void setupViewPager(boolean mostrar){
+    private void setupViewPager(boolean mostrar) {
         if (mostrar) {
             viewPager.setAdapter(new PagerAdapter(getSupportFragmentManager(), buildFragments()));
             tabLayout.setupWithViewPager(viewPager);
 
-            try{
+            try {
                 tabLayout.getTabAt(0).setIcon(R.drawable.ic_user);
                 tabLayout.getTabAt(1).setIcon(R.drawable.ic_car);
                 tabLayout.getTabAt(2).setIcon(R.drawable.ic_phone);
-            }catch (NullPointerException e){
+            } catch (NullPointerException e) {
                 e.printStackTrace();
             }
 
@@ -89,5 +101,34 @@ public class MainActivity extends AppCompatActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
+        Log.d("PAGE_SCROLLED", String.valueOf(position));
+    }
+
+    @Override
+    public void onPageSelected(int position) {
+        switch (position){
+            case 0:
+                editValue.setHint("Rut o Nombre completo");
+                break;
+            case 1:
+                editValue.setHint("Patente Ej: vk730");
+                break;
+            case 2:
+                editValue.setHint("IMEI del teléfono");
+                break;
+        }
+    }
+
+    @Override
+    public void onPageScrollStateChanged(int state) {
+        Log.d("PAGE_SCROLLSTATECHANGED", String.valueOf(state));
+    }
+
+    @OnClick(R.id.btn_buscar)
+    public void onClick() {
     }
 }
